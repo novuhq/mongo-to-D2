ccaebbe5ec60c95eda031ca04604e9fa897a7cc7
Zac Clifton <zac@novu.co>
2023-02-06 17:42:58 -0500

    Init

--- index.js
+++ index.js
@@ -1,35 +1,174 @@
-const fs = require("fs");
-const eta = require("eta");
-const pg = require("pg");
-const { execFileSync: sh } = require("child_process");
+import { readFileSync, writeFileSync} from 'fs';
+import {MongoClient, ObjectId, Long } from 'mongodb';
+import { execFileSync } from "child_process";
+import * as eta from "eta";
 
-const query = fs.readFileSync("./query.sql").toString();
-const template = fs.readFileSync("./template.eta").toString();
+const template = readFileSync("./template.eta").toString();
+
+async function listDatabases(client){
+  let databasesList = await client.db().admin().listDatabases();
+
+  console.log(databasesList.databases.length);
+
+  let dbList = databasesList.databases.filter(function (val) {
+    if (val.name !== "local" && val.name !== "config" && val.name !== "admin" ) {
+      return val
+    }
+  })
+
+  console.log("Databases:");
+  dbList.forEach(db => console.log(` - ${db.name}`));
+  return dbList;
+}
+
+function reduceElement(element, schema) {
+
+  for (const key of Object.keys(element)) {
+    if (!(key in schema)) {
+
+      let type = fieldTypeOf(element[key.toString()])
+
+      if (type === 'Object') {
+        schema[key.toString()] = {}
+        reduceElement(element[key.toString()], schema[key.toString()] )
+
+      } else if (type === 'Array') {
+
+        if ( (element[key.toString()].length === 0 || element[key.toString()][0] === 'undefined') && !(schema[key.toString()] instanceof Object) ) {
+
+          schema[key.toString()] = type + ' - undefined';
+
+        } else if (fieldTypeOf(element[key.toString()][0]) === 'Object') {
+
+          schema[key.toString()] = {}
+
+          reduceDocuments(element[key.toString()], schema[key.toString()] )
+
+        } else {
+          schema[key.toString()] = type + ' - ' + fieldTypeOf(element[key.toString()][0]);
+        }
+
+      } else {
+
+        schema[key.toString()] = type
+
+      }
+
+    } // if
+
+  }// for
+
+}
+
+function reduceDocuments(documentList, schema){
+
+  for (const doc of documentList) {
+
+    reduceElement(doc,schema)
+
+  }
+}
+
+function fieldTypeOf(thing) {
+  if (!arguments.length) { throw 'varietyTypeOf() requires an argument'; }
+
+  if (typeof thing === 'undefined') {
+    return 'undefined';
+  } else if (typeof thing !== 'object') {
+    // the messiness below capitalizes the first letter, so the output matches
+    // the other return values below. -JC
+    const typeofThing = typeof thing; // edgecase of JSHint's "singleGroups"
+    return typeofThing[0].toUpperCase() + typeofThing.slice(1);
+  } else {
+    if (thing && thing.constructor === Array) {
+      return 'Array';
+    } else if (thing === null) {
+      return 'null';
+    } else if (thing instanceof Date) {
+      return 'Date';
+    } else if(thing instanceof Long) {
+      return 'NumberLong';
+    } else if (thing instanceof ObjectId) {
+      return 'ObjectId';
+    // } else if (thing instanceof BSON.BSONType) {
+    //   const binDataTypes = {};
+    //   binDataTypes[0x00] = 'generic';
+    //   binDataTypes[0x01] = 'function';
+    //   binDataTypes[0x02] = 'old';
+    //   binDataTypes[0x03] = 'UUID';
+    //   binDataTypes[0x04] = 'UUID';
+    //   binDataTypes[0x05] = 'MD5';
+    //   binDataTypes[0x06] = 'Encrypted-BSON';
+    //   binDataTypes[0x07] = 'time-series';
+    //   binDataTypes[0x80] = 'user';
+    //   return 'BinData-' + binDataTypes[thing.subtype()];
+    } else {
+      return 'Object'
+    }
+  }
+}
+
 
 const args = process.argv.slice(2);
 
 async function getSchema() {
-  const { Client } = pg;
-  const client = new Client(args[0]);
+  let connectionString = "mongodb://localhost:27017"
+  if (args[0]){
+    connectionString = args[0]
+  }
+
+  const client = new MongoClient(connectionString);
+
   await client.connect();
 
-  const res = await client.query(query);
-  await client.end();
+  let dbList = await listDatabases(client);
 
-  return res.rows.map((result) => ({
-    ...result,
-    // postgres sometimes returns [null] for some reason
-    foreign_relations: result.foreign_relations.filter(
-      (relation) => !!relation
-    ),
-  }));
+  if (dbList.length === 0 ){
+    console.log("Unable to find any databases to diagram")
+    process.exit(1);
+  }
+
+  let pipeline = [
+    {
+      $sample:
+      /**
+       * size: The number of documents to sample.
+       */
+          {
+            size: 1000,
+          },
+    },
+  ]
+
+  let schema = {}
+
+  for (const database of dbList) {
+    schema[database.name.toString()] = {};
+
+    let collectionList = (await client.db(database.name.toString()).listCollections().toArray()).filter(obj => obj.type === "collection");
+
+    for (const collection of collectionList) {
+      const samples = await client.db(database.name).collection(collection.name).aggregate(pipeline).toArray();
+
+      schema[database.name][collection.name] = {}
+
+      reduceDocuments(samples, schema[database.name][collection.name]);
+
+    }
+  }
+
+  return schema
 }
 
 async function main() {
   const schema = await getSchema();
-  const output = eta.render(template, { schema });
-  fs.writeFileSync("output.d2", output);
-  sh("d2", ["output.d2", "out.svg"]);
+  const output = await eta.render(template, {schema});
+
+  writeFileSync("output.d2", output);
+  execFileSync("d2", ["output.d2", "out.svg"]);
+
+  process.exit(0);
+
 }
 
 main();
--- readme.md
+++ readme.md
@@ -1,8 +1,27 @@
-## [D2](https://github.com/terrastruct/d2#related) ERD from Postgres
+# Mongo to [D2](https://github.com/terrastruct/d2#related) (M2D2)
 
-![Example](/./example.png)
+## Maintained and supported by Novu
 
-[D2](https://github.com/terrastruct/d2) Diagrams are awesome. If you have a postgres database that already exists that you'd like to diagram, you can use this tool to generate an ERD.
+<div align="center">
+  <a href="https://novu.co" target="_blank">
+  <picture>
+    <source media="(prefers-color-scheme: dark)" srcset="https://user-images.githubusercontent.com/2233092/213641039-220ac15f-f367-4d13-9eaf-56e79433b8c1.png">
+    <img src="https://user-images.githubusercontent.com/2233092/213641043-3bbb3f21-3c53-4e67-afe5-755aeb222159.png" width="280" alt="Logo"/>
+  </picture>
+  </a>
+</div>
+
+[D2](https://github.com/terrastruct/d2) Diagrams are very useful know exactly what schemas your database is following.
+If you have a mongo database that you'd like to diagram, you can use this tool to generate a schema for it.
+
+Here is an example of the output of this tool rendered in Terrastruct. 
+
+![Example](./example.png)
+
+## This Tool Supports
+- Merging the same document referenced in other documents into the same table
+- Recursively mapping Arrays and Documents
+- Mapping only the user generated databases
 
 ## Installation
 
@@ -12,9 +31,14 @@
 ## Usage
 
 ```sh
-$ node index postgres://postgres:postgres@localhost:5432/yourdb
+$ node index "mongodb://localhost:27017"
 ```
 
-You will see that there are 2 gitignored files generated, `out.svg` and `output.d2`.
+## Future
 
-Happy hacking!
+We will look in the future to move this tool to a npm distributed cli tool for better maintainability
+and additional features.
+ 
+If you wish to contribute to this tool, <a href="https://discord.novu.co">Come Join Our Discord</a> 
+and discuss with the team what you would like to do. 
+
